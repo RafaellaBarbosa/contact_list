e04e718a36ec52b30d5ed6feca31f3d53fd0848e
Rafaella Barbosa <rafaellacsta@hotmail.com>
2024-10-21 20:55:27 -0300

    alterar url de user

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -1,29 +1,70 @@
 import '../models/user.dart';
 
-String url = 'https://i.pravatar.cc/300';
 
 final users = {
   '01': User(
     id: '1',
     name: 'Maria',
     email: 'maria@alunos.com.br',
-    avatarUrl: url,
+    avatarUrl:
+        "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png",
   ),
   '02': User(
     id: '2',
     name: 'Rafael',
     email: 'rafael@alunos.com.br',
-    avatarUrl: url,
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_1280.png',
   ),
   '03': User(
     id: '3',
     name: 'Fernanda',
     email: 'fernanda@gmail.com.br',
-    avatarUrl: url,
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/18/20/15/woman-995164_1280.png',
   ),
   '04': User(
-    id: '4',
-    name: 'Daniel',
-    email: 'daniel@gmail.com.br',
+      id: '4',
+      name: 'Daniel',
+      email: 'daniel@gmail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/10/avatar-1294776_1280.png'),
+  '05': User(
+    id: '5',
+    name: 'João',
+    email: 'joao@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_1280.png',
+  ),
+  '06': User(
+    id: '6',
+    name: 'Ana',
+    email: 'ana@alunos.com.br',
+  ),
+  '07': User(
+    id: '7',
+    name: 'Carlos',
+    email: 'carlos@gmail.com.br',
+  ),
+  '08': User(
+    id: '8',
+    name: 'Beatriz',
+    email: 'beatriz@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2022/02/23/20/17/man-7031423_1280.png',
+  ),
+  '09': User(
+    id: '9',
+    name: 'Pedro',
+    email: 'pedro@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/10/avatar-1294774_1280.png',
+  ),
+  '10': User(
+    id: '10',
+    name: 'Clara',
+    email: 'clara@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2023/02/16/10/14/woman-7793451_1280.png',
   ),
 };
