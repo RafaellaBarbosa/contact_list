dfeb97a44dfb601186b7ead546b3f35676af2b19
Rafaella Barbosa <rafaellacsta@hotmail.com>
2024-10-23 08:19:10 -0300

    Corrige inconsistência entre chaves e IDs no mock de usuários

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -1,59 +1,58 @@
 import '../models/user.dart';
 
-
 final users = {
-  '01': User(
+  '1': User(
     id: '1',
     name: 'Maria',
     email: 'maria@alunos.com.br',
     avatarUrl:
         "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png",
   ),
-  '02': User(
+  '2': User(
     id: '2',
     name: 'Rafael',
     email: 'rafael@alunos.com.br',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_1280.png',
   ),
-  '03': User(
+  '3': User(
     id: '3',
     name: 'Fernanda',
     email: 'fernanda@gmail.com.br',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2015/10/18/20/15/woman-995164_1280.png',
   ),
-  '04': User(
+  '4': User(
       id: '4',
       name: 'Daniel',
       email: 'daniel@gmail.com.br',
       avatarUrl:
           'https://cdn.pixabay.com/photo/2016/03/31/19/10/avatar-1294776_1280.png'),
-  '05': User(
+  '5': User(
     id: '5',
     name: 'João',
     email: 'joao@alunos.com.br',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_1280.png',
   ),
-  '06': User(
+  '6': User(
     id: '6',
     name: 'Ana',
     email: 'ana@alunos.com.br',
   ),
-  '07': User(
+  '7': User(
     id: '7',
     name: 'Carlos',
     email: 'carlos@gmail.com.br',
   ),
-  '08': User(
+  '8': User(
     id: '8',
     name: 'Beatriz',
     email: 'beatriz@gmail.com.br',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2022/02/23/20/17/man-7031423_1280.png',
   ),
-  '09': User(
+  '9': User(
     id: '9',
     name: 'Pedro',
     email: 'pedro@gmail.com.br',
